4e50a82fec1a5988c26da79b85a85cf3dc248287
Abhishek Gautam <121427611+abhishekgautam95@users.noreply.github.com>
2024-08-10 17:45:42 +0530

    Add files via upload

---
+++ Javascript_new_course/04_Comperision.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+// console.log("2">=1);
+// console.log("2a">="1")
+
+// console.log(null>=0);
+// console.log(null<=0);
+//output is true 
+
+console.log(null==null);
+console.log(null>0);
+console.log(null<0);
+console.log(null!=null);
+
+// console.log(0>=null);
+// console.log(0<=null);
+//output is true
+
+// console.log(undefined<0);
+// console.log(undefined<=0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+//output is false
+
+console.log("2"===1);
+console.log("3"===3);
+//output is false
+
+console.log("2"==2);
---
+++ Javascript_new_course/2Datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as  newer version
+
+// alert(3+3) //we are using node.js not browser 
+console.log(3+3) 
+console.log("Abishek");
+
+let name = "Abishek"
+let age = 22
+let isLoggedin = false
+let undefined = null
+let int = 0
+let String = "Abhishek Gautam"
+let boolean = false
+//number => 2 to poweer 53 
+//bigint 
+//String => "Abishek"
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//Symbol => unique 
+
+//object => 
+
+    console.log(typeof age);
+    console.log(typeof undefined);
+    console.log(typeof int);
+    console.log(typeof String);
+    console.log(typeof boolean);

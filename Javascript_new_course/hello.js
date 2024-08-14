01621dc9c993baf287075479fd5a9f1799121c21
Abhishek Gautam <121427611+abhishekgautam95@users.noreply.github.com>
2024-08-14 12:40:24 +0530

    Delete Javascript_new_course directory

--- Javascript_new_course/hello.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello world!");
-const accountID=142232
-let accountEmail = "abhigautam@gmail.com"
-var accountPassword = "12345"
-accountCity = "Sambhal"
-
-console.log(accountID)
-console.log(accountEmail)
-console.table([accountID,accountEmail,accountCity,accountPassword])

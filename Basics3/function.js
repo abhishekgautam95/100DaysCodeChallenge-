872b17e0628ef2a8ca12c0f7582e5e13ff4a1f5c
Abhishek Gautam <121427611+abhishekgautam95@users.noreply.github.com>
2024-08-16 16:34:47 +0530

    Add files via upload

---
+++ Basics3/function.js
@@ -0,0 +1,11 @@
+
+
+function writeMyname (){
+    console.log('A');
+    console.log('b');
+    console.log('h');
+    console.log('i');
+}
+
+// console.log(writeMyname);  [Function: writeMyname]
+writeMyname()
